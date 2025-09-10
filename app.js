e938090eeb00b2418559166b7fd2c734338d1aed
Jéssica Teixeira <jessicaferreirateixeira@gmail.com>
2025-09-09 23:47:25 -0300

    adiciona arquivos da calculadora simples com funções de operação e interface HTML

---
+++ app.js
@@ -0,0 +1,18 @@
+// Funções para operações matemáticas básicas
+function calculadora(operacao, a, b) {
+    switch (operacao) {
+        case 'soma':
+            return adicao(a, b);
+        case 'subtracao':
+            return subtracao(a, b);
+        case 'multiplicacao':
+            return multiplicacao(a, b);
+        case 'divisao':
+            return divisao(a, b);
+        default:
+            return "Operação inválida.";
+    }
+}
+
+calculadora();
+console.log(calculadora('soma', 5, 3));          // Saída: 8
